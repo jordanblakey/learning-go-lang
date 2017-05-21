f7dca9079681e035ed8a630d5b89e6572c074a0b
Jordan Blakey <jordan@jordanblakey.com>
2017-05-20 23:36:51 -0500

    manipulate files using os module

---
+++ herewego4.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+  "fmt"
+  "os"
+  "log"
+  "io/ioutil"
+)
+
+func main() {
+  file, err := os.Create("samp.txt")
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  file.WriteString("This is some random text.")
+  file.Close()
+
+  stream, err := ioutil.ReadFile("samp.txt")
+
+  if err != nil {
+    log.Fatal(err)
+  }
+
+  readString := string(stream)
+  fmt.Println(readString)
+
+}
+
+
+// "sort"
+// "os"
+// "io/ioutil"
+// "strconv"
